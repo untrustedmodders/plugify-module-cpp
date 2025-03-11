eb8cbda7c28856afabc4791cb1138eabdb6eb818
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2025-03-11 00:25:28 +0000

    fix: add extern to struct

--- include/plugify/api/plugify_api.hpp
+++ include/plugify/api/plugify_api.hpp
@@ -5,10 +5,12 @@
 namespace plg {
 	constexpr int kApiVersion = 1;
 
-	struct PluginContext {
-		bool hasUpdate{};
-		bool hasStart{};
-		bool hasEnd{};
-		bool hasDebug{};
-	};
+	extern "C" {
+		struct PluginContext {
+			bool hasUpdate{};
+			bool hasStart{};
+			bool hasEnd{};
+			bool hasDebug{};
+		};
+	}
 }
